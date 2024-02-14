75083ba80d1a0f8f69c57b6fb660273d45e6fec1
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-02-13 20:48:53 -0400

    OG Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,32 @@ import TransitionProvider from '@/components/transitionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
+/* How do I use and opengraph-image and twitter-image here? https://nextjs.org/docs/app/api-reference/file-conventions/metadata/opengraph-image */
+
 export const metadata = {
   title: 'Manuel Dev Portfolio App',
   description: 'Animated portfolio page',
+  openGraph: {
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 800,
+        height: 600,
+        alt: 'Manuel Dev Portfolio App',
+      },
+    ],
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+    images: [
+      {
+        url: '/twitter-image',
+        width: 800,
+        height: 600,
+        alt: 'Manuel Dev Portfolio App',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
